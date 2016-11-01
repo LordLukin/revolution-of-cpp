5a97589673f0e2b6705dacd777203b3265cf05e7
Łukasz Ziobroń <lukasz@ziobron.net>
2016-11-01 12:32:20 +0100

    'Almost' ready

--- src/cfront1_printf.hpp
+++ src/cfront1_printf.hpp
@@ -1,2 +1,3 @@
-int printf(const char* ...); // accept any argument after the initial character string
+int printf(const char* ...); // accept any argument after
+                             // the initial character string
 printf("date: %s %d 19%d\n", month, day, year); // maybe right
